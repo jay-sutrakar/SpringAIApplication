4f8f45557d32cd046281c898fc00e196cfc53935
jay-sutrakar <jaisutrakar@gmail.com>
2026-02-09 23:48:05 +0000

    update image controller to generate image on click

--- frontend/springaidemo_ui/src/components/ImageGeneratorComponent.jsx
+++ frontend/springaidemo_ui/src/components/ImageGeneratorComponent.jsx
@@ -2,6 +2,18 @@ import { useState } from "react";
 
 function ImageGeneratorComponent () {
     const [prompt, setPrompt] = useState('')
+    const [imgSrc, setImageSrc] = useState(null)
+    const generateImage = async () => {
+        try {
+            const response = await fetch('http://localhost:8080/generate-image?prompt=dog-image')
+            console.log(response);
+            const blob = await response.blob()
+            const url = URL.createObjectURL(blob)
+            setImageSrc(url)
+        } catch (error) {
+            console.error(error)
+        }
+    } 
    return (
     <div>
         <h2>Image Generator</h2>
@@ -11,6 +23,10 @@ function ImageGeneratorComponent () {
             value={prompt}
             placeholder="Enter the prompt to generate image."
         />
+        <button onClick={generateImage}>Generate Image</button>
+        <div>
+            <img src={imgSrc}/>
+        </div>
     </div>
    )
 }
